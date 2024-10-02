896d4b2679926174fc1f32a0bcc182843bc8bc66
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-10-02 14:09:11 +0300

    Initial Commit - script for restricting about author content

---
+++ restrictor.php
@@ -0,0 +1,18 @@
+if ( is_user_logged_in() ) : ?>
+  <p>
+    Mr. Nsiku's career includes significant experience with Amnesty International's East Africa Regional Office (EARO), where he focused on issues pertaining to the Democratic Republic of Congo. He now serves 
+    <span>
+      as the International Relations Manager at African Desk, a Pan-African organization Headquartered in Dar-es-Salaam, the United Republic of Tanzania. In addition, he works remotely as an Inspector for the Universal Action for the Defense of Human Rights, based in the Democratic Republic of Congo.
+    </span>
+  </p>
+<?php else : ?>
+  <p>
+    Mr. Nsiku's career includes significant experience with Amnesty International's East Africa Regional Office (EARO), where he focused on issues pertaining to the Democratic Republic of Kenya. He now serves 
+    <span class="blurred-text">
+      as the International Relations Manager at African Desk, a Pan-African organization Headquartered in Dar-es-Salaam, the United Republic of Tanzania. In addition, he works remotely as an Inspector for the Universal Action for the Defense of Human Rights, based in the Democratic Republic of Congo.
+    </span>
+    <br>
+    <a href="https://greatlakescrisis.org/register/" class="subscribe-link">Subscribe To View More</a>
+  </p>
+
+<?php endif;
