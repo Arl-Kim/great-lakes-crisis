5ebbab2dcec43e92739484fffc4914f2ede32e4c
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-10-02 15:25:52 +0300

    Move restricted content within containers and modify styling

--- restrictor.php
+++ restrictor.php
@@ -6,13 +6,19 @@ if ( is_user_logged_in() ) : ?>
     </span>
   </p>
 <?php else : ?>
-  <p>
-    Mr. Nsiku's career includes significant experience with Amnesty International's East Africa Regional Office (EARO), where he focused on issues pertaining to the Democratic Republic of Congo. He now serves 
-    <span class="blurred-text">
-      as the International Relations Manager at African Desk, a Pan-African organization Headquartered in Dar-es-Salaam, the United Republic of Tanzania. In addition, he works remotely as an Inspector for the Universal Action for the Defense of Human Rights, based in the Democratic Republic of Congo.
-    </span>
-    <a href="https://greatlakescrisis.org/register/" id="subscribe_link">Subscribe For More</a>
-  </p>
+  <div class="restricted-content">
+    <div class="restricted-paragraph">
+      <p>
+        Mr. Nsiku's career includes significant experience with Amnesty International's East Africa Regional Office (EARO), where he focused on issues pertaining to the Democratic Republic of Congo. He now serves 
+        <span class="blurred-text">
+          as the International Relations Manager at African Desk, a Pan-African organization Headquartered in Dar-es-Salaam, the United Republic of Tanzania. In addition, he works remotely as an Inspector for the Universal Action for the Defense of Human Rights, based in the Democratic Republic of Congo.
+        </span>
+      </p>
+    </div>
+    <div class="subscribe-link">
+      <a href="https://greatlakescrisis.org/register/" id="subscribe_link">Subscribe For More</a>
+    </div>
+  </div>
 
   <style>
     .blurred-text {
@@ -21,9 +27,17 @@ if ( is_user_logged_in() ) : ?>
       position: relative;
     }
 
-    #subscribe_link {
-      background: #053f5c;
-      color: #f27f0c;
+    .subscribe-link{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      bottom: 100px;
+    }
+
+    .subscribe-link #subscribe_link {
+      background: #f27f0c;
+      color: #053f5c;
       padding: 10px 30px;
       border-radius: 10px;
       font-weight: 700;
@@ -32,9 +46,9 @@ if ( is_user_logged_in() ) : ?>
       transition: all 400ms ease-in-out;
     }
 
-    #subscribe_link:hover {
-      background: #f27f0c;
-      color: #053f5c;
+    .subscribe-link #subscribe_link:hover {
+      background: #053f5c;
+      color: #f27f0c;
     }
   </style>
 
